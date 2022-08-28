dcacc174634926f86b126463fc611c7d36d7b13d
MaksimPyatak <maksimpyatakukr@gmail.com>
2022-08-28 16:50:58 +0300

    28.08

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,28 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-/*
+
 import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+
+const swiper = new Swiper('.first-section__swiper', {
+    modules: [Navigation, Pagination],
+    // Optional parameters
+    // direction: 'vertical',
+    // loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});

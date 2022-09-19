259b69c70cd453e81701f20e3b8f046243b5ccb1
MaksimPyatak <maksimpyatakukr@gmail.com>
2022-09-19 17:35:54 +0300

    19.09

--- src/js/Files/burger-menu.js
+++ src/js/Files/burger-menu.js
@@ -1,6 +1,7 @@
 const iconMenu = document.querySelector('.menu__burger');
 if (iconMenu){
-   const menuList = document.querySelector('.menu__list');
+   const menuBoxList = document.querySelector('.menu__box-list');
+   //const headerMenu = document.querySelector('.header__menu');
    const headerZero = document.querySelector('.header__zero');
    const firstSection = document.querySelector('.first-section__swiper');
    const menuBurgerIcon = document.querySelector('.menu__burger-icon');
@@ -8,9 +9,10 @@ if (iconMenu){
    iconMenu.addEventListener("click", function (e) {
       document.body.classList.toggle('_lock');
       menuBurgerIcon.classList.toggle('_active');
+      //headerMenu.classList.toggle('_active');
       headerZero.classList.toggle('_active');
       firstSection.classList.toggle('_active');
-      menuList.classList.toggle('_active');
+      menuBoxList.classList.toggle('_active');
       menyHeading.classList.toggle('_active');
    })
 }

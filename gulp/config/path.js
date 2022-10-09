b569d7d2d18f7fb0c640fe3885eebf989ecfc1f6
MaksimPyatak <maksimpyatakukr@gmail.com>
2022-10-09 10:28:06 +0300

    09.10 заміна папки dist на docs

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,8 +1,8 @@
-// Получаем имя папки проекта
+// Отримуємо ім'я для папки проекту
 import * as nodePath from 'path';
 const rootFolder = nodePath.basename(nodePath.resolve());
 
-const buildFolder = `./dist`; // Также можно использовать rootFolder
+const buildFolder = `./docs`; // Також можна використовувати rootFolder
 const srcFolder = `./src`;
 
 export const path = {
